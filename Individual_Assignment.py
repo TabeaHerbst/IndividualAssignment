1b0406efd459828c252d87d035fced4b08bffd0e
TabeaHerbst <131797468+TabeaHerbst@users.noreply.github.com>
2023-11-10 22:29:55 +0100

    Individual_Assignment.py

--- Individual_Assignment.py
+++ Individual_Assignment.py
@@ -181,10 +181,9 @@ def main():
                 f'The water is {prediction_label_rf}.'
                 f'</p></div>', unsafe_allow_html=True)
 
-    
     st.sidebar.markdown("<br><br><br><br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-    st.sidebar.markdown('<p style="font-size: smaller;">[Pumpkin Seeds Source Datasets on GitHub](https://github.com/TabeaHerbst/IndividualAssignment/blob/main/Pumpkin_Seeds_Dataset.csv)</p>', unsafe_allow_html=True)
-    st.sidebar.markdown('<p style="font-size: smaller;">[Water Potability Source Datasets on GitHub](https://github.com/TabeaHerbst/IndividualAssignment/blob/main/water_potability.csv)</p>', unsafe_allow_html=True)
+    st.sidebar.markdown('<a style="font-size: smaller;" href="https://github.com/TabeaHerbst/IndividualAssignment/blob/main/Pumpkin_Seeds_Dataset.csv" target="_blank">Pumpkin Seeds Source Datasets on GitHub</a>', unsafe_allow_html=True)
+    st.sidebar.markdown('<a style="font-size: smaller;" href="https://github.com/TabeaHerbst/IndividualAssignment/blob/main/water_potability.csv" target="_blank">Water Potability Source Datasets on GitHub</a>', unsafe_allow_html=True)
 
     
 
